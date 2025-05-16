bf17598c74d3e904541cb7e4ad5e04f572ed2e87
kushagra singhal <kushagrasinghal987@gmail.com>
2025-05-16 17:00:52 +0530

    Made about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ function About () {
   return (
     <div className='w-full p-15 bg-[#CDEA68] text-black'> 
     <h1 className='font-["NeueMontreal"] text-[4.5vw] leading-[4.5vw] tracking-tight'>Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell products, explain complex ideas, and hire great people. </h1>
+     
           </div>
   )
 }
